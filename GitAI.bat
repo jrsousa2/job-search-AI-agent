09ebf99ec610c32dd05951e2ece88b3ffebb11a0
Jose Risomar Sousa <jrsousa2@hotmail.com>
2026-07-12 13:20:41 -0300

    Updt

--- GitAI.bat
+++ GitAI.bat
@@ -16,7 +16,7 @@ IF "%~1"=="" (
  cd D:\Agent
  set GIT_TRACE=1
  echo TO FORCE A RE-READING OF THE .gitignore list uncomment the next line
- rem git rm -r --cached .
+ git rm -r --cached .
  git add -A
  REM git branch -M main
  git commit -m "%Desc%"
