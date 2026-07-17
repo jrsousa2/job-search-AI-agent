06718a386349549c20a46901e4eade463ac7b2b3
Jose Risomar Sousa <jrsousa2@hotmail.com>
2026-07-17 12:42:04 -0300

    Updt

--- Run_check_boards.bat
+++ Run_check_boards.bat
@@ -1,3 +1,3 @@
 @echo off
 REM RUNS THE PGM AND SAVES OUTPUT TO LOG
-C:\Python\Python3.11-v1\Scripts\python.exe D:\Agent\scripts\check_boards.py > D:\Agent\l=Logs\check_boards_log.txt 2>&1
+C:\Python\Python3.11-v1\Scripts\python.exe D:\Agent\scripts\check_boards.py > D:\Agent\Logs\check_boards_log.txt 2>&1
