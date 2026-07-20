47934f4d6d74b751ba1400134345efaded367d15
Jose Risomar Sousa <jrsousa2@hotmail.com>
2026-07-19 22:29:21 -0300

    Updt

--- scripts/Update_TZ_by_state.py
+++ scripts/Update_TZ_by_state.py
@@ -1,7 +1,6 @@
 import sqlite3
 
 from Repo_root import DB_FILE
-from TZ_codes import get_timezone
 
 ET_states = """
     (instr(location,'CT')>0) OR location LIKE '%Connecticut%'
@@ -29,11 +28,55 @@ OR  (instr(location,'WV')>0) OR location LIKE '%West Virginia%'
 OR  (instr(location,'DC')>0) OR location LIKE '%District of Columbia%'
 """
 
+ET_cities = """
+    location LIKE '%Alexandria%'
+OR  location LIKE '%Albany%'
+OR  location LIKE '%Ann Arbor%'
+OR  location LIKE '%Arlington%'
+OR  location LIKE '%Atlanta%'
+OR  location LIKE '%Baltimore%'
+OR  location LIKE '%Boston%'
+OR  location LIKE '%Buffalo%'
+OR  location LIKE '%Charleston%'
+OR  location LIKE '%Charlotte%'
+OR  location LIKE '%Cincinnati%'
+OR  location LIKE '%Cleveland%'
+OR  location LIKE '%Columbus%'
+OR  location LIKE '%Detroit%'
+OR  location LIKE '%Durham%'
+OR  location LIKE '%Fort Lauderdale%'
+OR  location LIKE '%Hartford%'
+OR  location LIKE '%Indianapolis%'
+OR  location LIKE '%Jacksonville%'
+OR  location LIKE '%Jersey City%'
+OR  location LIKE '%Knoxville%'
+OR  location LIKE '%Lexington%'
+OR  location LIKE '%Louisville%'
+OR  location LIKE '%Miami%'
+OR  location LIKE '%Nashville%'
+OR  location LIKE '%New York%'
+OR  location LIKE '%Newark%'
+OR  location LIKE '%Norfolk%'
+OR  location LIKE '%NYC%'
+OR  location LIKE '%Orlando%'
+OR  location LIKE '%Philadelphia%'
+OR  location LIKE '%Pittsburgh%'
+OR  location LIKE '%Providence%'
+OR  location LIKE '%Raleigh%'
+OR  location LIKE '%Richmond%'
+OR  location LIKE '%Rochester%'
+OR  location LIKE '%Savannah%'
+OR  location LIKE '%Tampa%'
+OR  location LIKE '%Washington%'
+OR  location LIKE '%Washington DC%'
+OR  location LIKE '%West Palm Beach%'
+"""
+
 def Update_TZ(DB_FILE):
     conn = sqlite3.connect(DB_FILE)
     cursor = conn.cursor()
 
-    print("Updating timezone...")
+    print("Updating timezone by state...")
     # ADD COLUMN IF MISSING
     cursor.execute("PRAGMA table_info(new_jobs)")
     columns = [row[1] for row in cursor.fetchall()]
@@ -41,62 +84,23 @@ def Update_TZ(DB_FILE):
     if "TZ" not in columns:
         cursor.execute("ALTER TABLE new_jobs ADD COLUMN TZ TEXT")
 
-    # CREATE CACHE TABLE
-    cursor.execute("CREATE TABLE IF NOT EXISTS location_TZ (location TEXT PRIMARY KEY,TZ TEXT)")
-
-    # GET DISTINCT CLEAN LOCATIONS
-    cursor.execute("""
-        SELECT DISTINCT lower(trim(location))
-        FROM new_jobs
-        WHERE location is not null
-    """)
-
-    locations = [row[0] for row in cursor.fetchall()]
-
-    print(f"Unique locations found: {len(locations)}")
-
-    # GET LOCATIONS NOT ALREADY PROCESSED
-    cursor.execute("""
-        SELECT location
-        FROM location_TZ
-    """)
-
-    existing = {row[0] for row in cursor.fetchall()}
-
-    new_locations = [loc for loc in locations if loc not in existing]
-
-    print(f"New locations to geocode: {len(new_locations)}")
-
-
-    # GEOCODE NEW LOCATIONS ONLY
-    timezone_rows = []
-    for location in new_locations:
-        tz = get_timezone(location)
-        print(location, "->", tz)
-        timezone_rows.append( (location, tz))
-
-    # SAVE CACHE
-    cursor.executemany("""
-        INSERT OR REPLACE INTO location_TZ
-        (location, TZ)
-        VALUES (?, ?)
-    """, timezone_rows)
-
-
     # UPDATE JOBS TABLE
-    cursor.execute("""
-        UPDATE new_jobs
-        SET TZ =
-        (SELECT TZ
-         FROM location_TZ lt
-         WHERE lt.location = LOWER(TRIM(new_jobs.location))
-        )""")
+    cursor.execute(f"""
+    UPDATE new_jobs
+    SET TZ = CASE
+        WHEN ({ET_states}) OR ({ET_cities})
+        THEN 'ET'
+        ELSE (
+            SELECT TZ
+            FROM location_TZ lt
+            WHERE lt.location = LOWER(TRIM(new_jobs.location))
+        ) END""")
 
     # CLOSE CONNECTION
     conn.commit()
     conn.close()
 
-    print("Timezone update complete.")
+    print("Timezone update based on state/cities complete.")
 
 # CALL FUNCTION
 if __name__ == "__main__":
